cdc9d3203fb8d2e9371de5be72e320d15a6146cd
Jona L <jonathan17845@gmail.com>
2024-10-16 14:15:15 +0700

    add index and login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/aset/css/styles.css">
+</head>
+<body>
+    <div class="ayok">
+        <center>
+    <form id="loginForm">
+        <div>
+            <label for="username">Username : </label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Password : </label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit">LOGIN</button>
+    </center>
+    </form>
+    <script src="aset/js/script.js"></script>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/welcome', function () {
+    return view('index'); //merujuk ke file index.blade.php di folder resource
+});
+
+Route::get('/login', function () {
+    return view('login'); //merujuk ke file index.blade.php di folder resource
+});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //merujuk ke file welcome.blade.php di folder resource
 });
